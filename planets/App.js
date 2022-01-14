dcf26dfb506e84da27e158cabab54d0abef2483c
Tim Maas Geesteranus <tim__mg@hotmail.com>
2022-01-14 16:44:50 +0100

    add hello world app

---
+++ planets/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import type { Node } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+const App: () => Node = () => {
+
+  return (
+    <View>
+
+      <Text>Hello World!</Text>
+
+
+
+    </View>
+  );
+};
+
+export default App;
