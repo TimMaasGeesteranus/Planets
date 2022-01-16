50a8c1c7363a0e46cb4781dc28314b6d405deb34
Tim Maas Geesteranus <tim__mg@hotmail.com>
2022-01-16 18:33:09 +0100

    cleanup

--- planets/App.js
+++ planets/App.js
@@ -1,14 +1,6 @@
 import React from 'react';
 
-import {
-  View,
-  Text,
-  ImageBackground,
-  StyleSheet
-} from 'react-native';
 import StartScreen from './components/screens/StartScreen.js';
-
-const image = require('./public/stars.gif')
 
 const App = () => {
 
